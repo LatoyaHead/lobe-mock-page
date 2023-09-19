2c0d71f66fb71f30fb8d741f45c1f8fa4bd4a455
24HourDeveloper <latoya30310@gmail.com>
2023-09-19 14:07:07 -0400

    added dropdown menu for smaller screens

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector('.dropbtn')
+const list = document.querySelector('.dropdown-content')
+
+menuBtn.addEventListener('click', (e)=> {
+if (list.style.display === 'none') {
+  list.style.display = 'block'
+} else {
+  list.style.display = 'none'
+}
+})
+
+
+
